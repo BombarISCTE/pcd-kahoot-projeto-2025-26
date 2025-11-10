854e14db243b8cdbdd91449f5dc521970043d8ff
JoaoLouro <jmmlo1@iscte-iul.pt>
2025-11-10 22:50:31 +0000

    Completar classe GeradorCodigo
    Criação do método gerarCodigo, para conseguirmos gerar valores entre 0 e 999999, que irá ser responsável pela criação dos códigos dos nossos jogos
    
    Criação de uma coleção HashSet de forma a que não exista repetição dos códigos
    
    Criação do metodo apagarCodigo, serve tal como o nome indica para podermos eliminar o código de um jogo
    
    Nota: no método gerarCodigo, %06d irá servir para manter o nosso código sempre com 6digitos, mesmo que o código gerado tenha sido 55, ele adiciona os restantes 4 dígitos a 0, á esquerda, ficando por exemplo 000055

--- src/Server/GeradorCodigo.java
+++ src/Server/GeradorCodigo.java
@@ -1,4 +1,31 @@
 package Server;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+//2.2 Códigos do jogo
+//Servidor deve ser capaz de gerar um código único que não esteja em utilização.
+//O servidor deve conseguir armazenar os códigos e descartá-los quando
+//não estão mais em utilização.
 public class GeradorCodigo {
+
+    private static final Random random = new Random();
+    private static final Set<String> codigosUsados = new HashSet<String>();
+
+    public static synchronized String gerarCodigo(){
+        String codigo;
+        while(true){
+            codigo = String.format("%06d", random.nextInt(1000000));
+            if(!codigosUsados.contains(codigo)){
+                codigosUsados.add(codigo);
+                return codigo;
+            }
+        }
+    }
+
+    public static synchronized void apagarCodigo(String codigo) {
+        codigosUsados.remove(codigo);
+    }
+
 }
