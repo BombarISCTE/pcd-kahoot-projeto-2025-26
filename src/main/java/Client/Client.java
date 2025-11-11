b8756b3ef1cfeb79a46c8837c2d4b0bc545d9213
raulf <rfkao@iscte-iul.pt>
2025-11-11 19:15:35 +0000

    some client server logic added

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -2,7 +2,16 @@ package Client;
 
 import Game.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
 
     //usado para entrar no jogo ?
     public Client(int gameID, Team team, int ClientId, String address, int port) {
@@ -13,4 +22,25 @@ public class Client {
     public Client (int IP , int Port, Game game, Team team, String username){
 
     }
+
+    public void runClient(){
+        try{
+            connectToServer();
+            sendMessages();
+        } catch (IOException _){
+            //System.err.println("IO Exception thrown");
+            throw new IllegalStateException("sys kaboom");
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException _ ){
+                System.err.println("IO Exception thrown");
+            }
+        }
+    }
+
+//    public static void main( String[] args){
+//        new Client().runClient();
+//    }
+
 }
