37525396079360e1a08911a81dc54624b930b642
JoaoLouro <jmmlo1@iscte-iul.pt>
2025-11-12 20:17:08 +0000

    alteraçoes

--- src/main/java/Client/clientKahoot.java
+++ src/main/java/Client/clientKahoot.java
@@ -4,19 +4,21 @@ import Game.Team;
 
 public class clientKahoot {
     public static void main(String[] args) {
-        if (args.length < 5) {
-            System.out.println("Uso: java clientKahoot <IP> <PORT> <GameCode> <TeamCode> <Username>");
-            return;
-        }
+//        if (args.length < 5) {
+//            System.out.println("Uso: java clientKahoot <IP> <PORT> <GameCode> <TeamCode> <Username>");
+//            return;
+//        }
+        ClientGUI c = new ClientGUI();
+
 
         String ip = args[0];
         int port = Integer.parseInt(args[1]);
         String gameCode = args[2];
         //String teamCode = args[3];
-        Team team = new Team(args[3]);
-        String username = args[4];
-
-        Client client = new Client(ip, port, gameCode, teamCode, username);
-        client.start();
+//        Team team = new Team(args[3]);
+//        String username = args[4];
+//
+//        Client client = new Client(ip, port, gameCode, teamCode, username);
+//        client.start();
     }
 }
