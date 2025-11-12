98f4321deab5584f9cd9864bb9ec4a8c83e7ac28
JoaoLouro <jmmlo1@iscte-iul.pt>
2025-11-12 15:33:39 +0000

    Alteração do random
    Alteração do random por ThreadLocalRandom.current()

--- src/main/java/Server/GeradorCodigo.java
+++ src/main/java/Server/GeradorCodigo.java
@@ -3,6 +3,7 @@ package Server;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 //2.2 Códigos do jogo
 //Servidor deve ser capaz de gerar um código único que não esteja em utilização.
@@ -10,13 +11,12 @@ import java.util.Set;
 //não estão mais em utilização.
 public class GeradorCodigo {
 
-    private static final Random random = new Random();
     private static final Set<String> codigosUsados = new HashSet<String>();
 
     public static synchronized String gerarCodigo(){
         String codigo;
         while(true){
-            codigo = String.format("%06d", random.nextInt(1000000));
+            codigo = String.format("%06d", ThreadLocalRandom.current().nextInt(1000000));
             if(!codigosUsados.contains(codigo)){
                 codigosUsados.add(codigo);
                 return codigo;
