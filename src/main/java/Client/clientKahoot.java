22df2eb8951276ae83ca1483211618ad99d80509
JoaoLouro <jmmlo1@iscte-iul.pt>
2025-11-15 19:14:26 +0000

    Comentei a clientGUI
    Comentei uma parte da clientGUI para testar a gui com a 1ºpergunta

--- src/main/java/Client/clientKahoot.java
+++ src/main/java/Client/clientKahoot.java
@@ -6,20 +6,20 @@ import Game.Team;
 public class clientKahoot {
 
     public static void main(String[] args) {
-        if (args.length < 5) {
-            System.out.println("Uso: java clientKahoot <IP> <PORT> <GameCode> <TeamCode> <Username>");
-            return;
-        }
-
-        String ip = args[0];
-        int port = Integer.parseInt(args[1]);
-        String gameCode = args[2];
-        //String teamCode = args[3];
-        Team team = new Team(Integer.parseInt(args[3]));
-        String username = args[4];
-
-        //Client client = new Client(ip, port, gameCode, team, username);
-        //client.runClient();
+//        if (args.length < 5) {
+//            System.out.println("Uso: java clientKahoot <IP> <PORT> <GameCode> <TeamCode> <Username>");
+//            return;
+//        }
+//
+//        String ip = args[0];
+//        int port = Integer.parseInt(args[1]);
+//        String gameCode = args[2];
+//        //String teamCode = args[3];
+//        Team team = new Team(Integer.parseInt(args[3]));
+//        String username = args[4];
+//
+//        //Client client = new Client(ip, port, gameCode, team, username);
+//        //client.runClient();
 
         try{
             Pergunta[] perguntas = Pergunta.lerPerguntas("src/main/java/Game/FicheiroQuestoes.json");
